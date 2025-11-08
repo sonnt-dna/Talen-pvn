2c734a6e715756195411c4e7dba109a6d6c2be27
sonnt-dna <110382822+sonnt-dna@users.noreply.github.com>
2025-11-08 20:38:36 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
-      allowedHosts: ['talen-pvn.onrender.com'], // 👈 thêm dòng này
+      allowedHosts: 'all', // 👈 thêm dòng này
       // hoặc: allowedHosts: ['talen-pvn.onrender.com', 'localhost']
       // hoặc cho tất cả: allowedHosts: 'all'
     },
